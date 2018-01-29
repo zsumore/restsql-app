d99dc3a1a09fec44903d13edb351b18ce5e389b0
cinrad <35585144@qq.com>
2018-01-29 20:45:13 +0800

    fix

--- src/main/java/mq/restful/util/MyFilterToSQL.java
+++ src/main/java/mq/restful/util/MyFilterToSQL.java
@@ -59,9 +59,8 @@ public class MyFilterToSQL extends FilterToSQL {
 		// TODO Auto-generated method stub
 		if (literal == null) {
 			out.write("NULL");
-		} else if (literal instanceof String ) {
-			String encoding = (String) Converters.convert(literal,
-					String.class, null);
+		} else if (literal instanceof String) {
+			String encoding = (String) Converters.convert(literal, String.class, null);
 			if (encoding == null) {
 				// could not convert back to string, use original l value
 				encoding = literal.toString();
@@ -70,18 +69,15 @@ public class MyFilterToSQL extends FilterToSQL {
 			// sigle quotes must be escaped to have a valid sql string
 			String escaped = encoding.replaceAll("'", "''");
 			out.write("'" + escaped + "'");
-		}
-		else if (literal instanceof Number || literal instanceof Boolean) {
+		} else if (literal instanceof Number || literal instanceof Boolean) {
 			System.out.println(String.valueOf(literal));
 			out.write(String.valueOf(literal));
 		} else if (literal instanceof Date) {
-			String valueDate = getDateFormat().format(literal).replaceAll("'",
-					"''");
+			String valueDate = getDateFormat().format(literal).replaceAll("'", "''");
 			out.write("'" + valueDate + "'");
-		}else {
+		} else {
 			// we don't know the type...just convert back to a string
-			String encoding = (String) Converters.convert(literal,
-					String.class, null);
+			String encoding = (String) Converters.convert(literal, String.class, null);
 			if (encoding == null) {
 				// could not convert back to string, use original l value
 				encoding = literal.toString();
@@ -92,98 +88,96 @@ public class MyFilterToSQL extends FilterToSQL {
 			out.write("'" + escaped + "'");
 		}
 	}
-	
-	 /**
-     * Encodes an Id filter
-     *
-     * @param filter the
-     *
-     * @throws RuntimeException If there's a problem writing output
-     *
-     */
+
+	/**
+	 * Encodes an Id filter
+	 *
+	 * @param filter
+	 *            the
+	 *
+	 * @throws RuntimeException
+	 *             If there's a problem writing output
+	 *
+	 */
 	@Override
-    public Object visit(Id filter, Object extraData) {
-        if (mapper == null) {
-            throw new RuntimeException(
-                "Must set a fid mapper before trying to encode FIDFilters");
-        }
+	public Object visit(Id filter, Object extraData) {
+		if (mapper == null) {
+			throw new RuntimeException("Must set a fid mapper before trying to encode FIDFilters");
+		}
 
-        Set ids = filter.getIdentifiers();
-        
-        //LOGGER.finer("Exporting FID=" + ids);
+		Set<Identifier> ids = filter.getIdentifiers();
 
-        // prepare column name array
-        String[] colNames = new String[mapper.getColumnCount()];
+		// LOGGER.finer("Exporting FID=" + ids);
 
-        for (int i = 0; i < colNames.length; i++) {
-            colNames[i] = mapper.getColumnName(i);
-        }
+		// prepare column name array
+		String[] colNames = new String[mapper.getColumnCount()];
 
-        try {
-            if (ids.size() > 1) {
-                out.write("(");
-            }
-            for (Iterator i = ids.iterator(); i.hasNext();) {
-                Identifier id = (Identifier) i.next();
-                Object[] attValues = mapper.getPKAttributes(id.toString());
+		for (int i = 0; i < colNames.length; i++) {
+			colNames[i] = mapper.getColumnName(i);
+		}
 
-                out.write("(");
+		try {
+			if (ids.size() > 1) {
+				out.write("(");
+			}
+			for (Iterator<Identifier> i = ids.iterator(); i.hasNext();) {
+				Identifier id = i.next();
+				Object[] attValues = mapper.getPKAttributes(id.toString());
 
-                for (int j = 0; j < attValues.length; j++) {
-                    out.write( escapeName(colNames[j]) );
-                    out.write(" = '");
-                    out.write(attValues[j].toString()); //DJB: changed this to attValues[j] from attValues[i].
-                    out.write("'");
+				out.write("(");
 
-                    if (j < (attValues.length - 1)) {
-                        out.write(" AND ");
-                    }
-                }
+				for (int j = 0; j < attValues.length; j++) {
+					out.write(escapeName(colNames[j]));
+					out.write(" = '");
+					out.write(attValues[j].toString()); // DJB: changed this to attValues[j] from attValues[i].
+					out.write("'");
 
-                out.write(")");
+					if (j < (attValues.length - 1)) {
+						out.write(" AND ");
+					}
+				}
 
-                if (i.hasNext()) {
-                    out.write(" OR ");
-                }
-            }
-            if (ids.size() > 1) {
-                out.write(")");
-            }
-        } catch (java.io.IOException e) {
-            throw new RuntimeException(IO_ERROR, e);
-        }
-        
+				out.write(")");
 
-        return extraData;
-    }
+				if (i.hasNext()) {
+					out.write(" OR ");
+				}
+			}
+			if (ids.size() > 1) {
+				out.write(")");
+			}
+		} catch (java.io.IOException e) {
+			throw new RuntimeException(IO_ERROR, e);
+		}
+
+		return extraData;
+	}
 
 	@Override
 	protected FilterCapabilities createFilterCapabilities() {
-		 FilterCapabilities capabilities = new FilterCapabilities();
+		FilterCapabilities capabilities = new FilterCapabilities();
 
-	        capabilities.addAll(FilterCapabilities.LOGICAL_OPENGIS);
-	        capabilities.addAll(FilterCapabilities.SIMPLE_COMPARISONS_OPENGIS);
-	        capabilities.addType(PropertyIsNull.class);
-	        capabilities.addType(PropertyIsBetween.class);
-	        capabilities.addType(Id.class);
-	        capabilities.addType(IncludeFilter.class);
-	        capabilities.addType(ExcludeFilter.class);
-	        capabilities.addType(PropertyIsLike.class);
-	        
-	        //temporal filters
-	        capabilities.addType(After.class);
-	        capabilities.addType(Before.class);
-	        capabilities.addType(Begins.class);
-	        capabilities.addType(BegunBy.class);
-	        capabilities.addType(During.class);
-	        capabilities.addType(Ends.class);
-	        capabilities.addType(EndedBy.class);
-	        capabilities.addType(TContains.class);
-	        capabilities.addType(TEquals.class);
+		capabilities.addAll(FilterCapabilities.LOGICAL_OPENGIS);
+		capabilities.addAll(FilterCapabilities.SIMPLE_COMPARISONS_OPENGIS);
+		capabilities.addType(PropertyIsNull.class);
+		capabilities.addType(PropertyIsBetween.class);
+		capabilities.addType(Id.class);
+		capabilities.addType(IncludeFilter.class);
+		capabilities.addType(ExcludeFilter.class);
+		capabilities.addType(PropertyIsLike.class);
 
-	        return capabilities;
+		// temporal filters
+		capabilities.addType(After.class);
+		capabilities.addType(Before.class);
+		capabilities.addType(Begins.class);
+		capabilities.addType(BegunBy.class);
+		capabilities.addType(During.class);
+		capabilities.addType(Ends.class);
+		capabilities.addType(EndedBy.class);
+		capabilities.addType(TContains.class);
+		capabilities.addType(TEquals.class);
+
+		return capabilities;
 	}
-	
-	
 
 }
